5a71b26dd253e9ee5bbdb9bdfe217899158035db
jean-godoy <seidecapital@gmail.com>
2021-01-07 23:29:33 -0300

    resolve

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// const express = require('express');
-// const {} = require('path');
-// const app = express();
+const express = require('express');
+const { resolve } = require('path');
+const app = express();
 
-// app.use('/', express.static(
-//     resolve(__dirname, './build')
-// ));
+app.use('/', express.static(
+    resolve(__dirname, './build')
+));
 
-// app.listen(process.env.PORT || 3000 , (e) => {
-//     if(e) {
-//         return console.log(e);
-//     }
+app.listen(process.env.PORT || 3000 , (e) => {
+    if(e) {
+        return console.log(e);
+    }
 
-//     console.log('Server Started..!');
-// });
-
-console.log('batata')
+    console.log('Server Started..!');
+});
