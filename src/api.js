1d294e362f63520971c8f256971f184bd32a5bb2
jean-godoy <70454706+jean-godoy@users.noreply.github.com>
2021-01-08 12:37:05 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://www.fenix-faccoes.herokuapp.com',
+    baseURL: 'https://fenix-faccoes.herokuapp.com',
 });
 
 export default api;
