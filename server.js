5a8615e249095b808440f683d47d701b0afa238f
jean-godoy <seidecapital@gmail.com>
2021-01-07 22:56:20 -0300

    config express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {} = require('path');
+const app = express();
+
+app.use('/', express.static(
+    resolve(__dirname, './build');
+));
+
+app.listen(process.env.PORT || 3000 , (e) => {
+    if(e) {
+        return console.log(e);
+    }
+
+    console.log('Server Started..!');
+});
