049f3beafa5bfcefe38a28d4732a3fb1c5ff6c4f
jean-godoy <seidecapital@gmail.com>
2021-01-07 23:00:37 -0300

    erro de ;

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const {} = require('path');
 const app = express();
 
 app.use('/', express.static(
-    resolve(__dirname, './build');
+    resolve(__dirname, './build')
 ));
 
 app.listen(process.env.PORT || 3000 , (e) => {
