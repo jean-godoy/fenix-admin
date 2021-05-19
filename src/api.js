8824d6903abe6ba1f44265f44489229f452875c6
jean-godoy <seidecapital@gmail.com>
2021-05-19 16:46:59 -0300

    alteracao do env para prod

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ import env from "react-dotenv";
 
 const api = axios.create({
     // baseURL: env.REACT_APP_URL,
-    baseURL: 'http://localhost:8000',
-    // baseURL: 'https://fenix-faccoes-api.herokuapp.com',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://fenix-faccoes-api.herokuapp.com',
 });
 
 export default api;
