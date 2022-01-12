b8b40dab4bdf44cf4f2f584deab3719ba1414e54
jean-godoy <seidecapital@gmail.com>
2022-01-12 17:01:06 -0300

    finalizacao da parte de edicao de faccoes

--- src/pages/Faccoes/FaccaoEdit.jsx
+++ src/pages/Faccoes/FaccaoEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiEdit } from 'react-icons/fi';
 import api from '../../api';
 
@@ -15,6 +15,7 @@ export default props => {
     const id = props.match.params.id;
     const [data, setData] = useState([]);
     const [faccaoCode, setFaccaoCode] = useState(null);
+    const history = useHistory();
 
     //maskers
     const [phone, setPhone] = useState('');
@@ -30,10 +31,31 @@ export default props => {
         }).catch(e => {
             return alert('Nenhuma Faccção corresponde está id');
         });
-    },[]);
+    }, []);
 
-    const onSubmit = () => {
-
+    //função que manda os dados editados para API.
+    const onSubmit = e => {
+        e.preventDefault()
+        const data_values = {
+            id: id,
+            faccao_name: data.faccaoName,
+            phone: unMask(phone),
+            street: data.street,
+            cpf: unMask(cpf),
+            employees: data.employees,
+            cidade: data.cidade,
+            bairro: data.bairro,
+            numero: data.numero
+        }
+        const data_string = JSON.stringify(data_values);
+        api.patch(`/faccoes/update/${id}`, data_string).then((res) => {
+            if (res.status === 200) {
+                alert("Facção: " + data.faccaoName + ", editado com sucesso!");
+                return history.push('/faccoes');
+            }
+        }).catch((e) => {
+            alert("Ops.., Erro ao editar usuario..");
+        });
     }
 
     //função responsavel dela edição do valor.
@@ -42,16 +64,16 @@ export default props => {
         setData({ ...data, [name]: value });
     }
 
-    //funçao responsavel por editar o0 telefone.
+    //funçao responsavel por editar o telefone.
     const phoneChange = e => {
-        setPhone(e.target.value);
+        setPhone(mask(unMask(e.target.value), ['(99) 9999-9999', '(99) 9 9999-9999']));
     }
 
     //função responsavel por editar o CPF.
     const cpfChange = e => {
-        setCpf(e.target.value);
+        setCpf(mask(unMask(e.target.value), ['999.999.999-99']));
     }
-   
+
     return (
         <>
             <Menu />
@@ -124,15 +146,10 @@ export default props => {
 
                             <div className="btn-group">
                                 <Link className="button" to="/faccoes">Cancelar</Link>
-                                <button className="button" type="submit">Cadastrar</button>
+                                <button className="button" type="submit">Editar</button>
                             </div>
 
                         </form>
-
-                        <div className="btn-group">
-                            <Link className="button" to={`/faccao-edit/${id}`}>Editar</Link>
-                            <Link className="button" to="/faccoes">OK</Link>
-                        </div>
 
                     </div>
 
